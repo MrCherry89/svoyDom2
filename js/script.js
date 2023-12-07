6b34d6a00d720a9c9172dad42d7fb92ec5558497
MrCherry89 <abgarnati@yandex.ru>
2023-12-08 03:09:28 +0400

    sdelal blok

--- js/script.js
+++ js/script.js
@@ -370,13 +370,11 @@ $(document).ready(function () {
     onComplete: function () {
       $(".gsap-wrapper").addClass("hidden");
       $(".animate-wrap").addClass("visible");
-      // $(".header").removeClass("fix");
     },
     onUpdate: function () {
       if ($(".gsap-wrapper").hasClass("hidden")) {
         $(".gsap-wrapper").removeClass("hidden");
         $(".animate-wrap").removeClass("visible");
-        // $(".header").addClass("fix");
       }
     },
   });
@@ -400,105 +398,24 @@ $(document).ready(function () {
     },
   });
 
-  // gsap.to(".our-winners .wrapper h2", {
-  //   // y: "50%",
-  //   scale: 0.5,
-  //   duration: 1,
-  //   scrollTrigger: {
-  //     trigger: ".animate-wrap",
-  //     scrub: -1,
-  //     start: "top top",
-  //   },
-  // });
-
-  // gsap.to(".our-winners", {
-  //   y: "-50%",
-  //   scale: 0.5,
-  //   duration: 1,
-  //   scrollTrigger: {
-  //     trigger: ".animate-wrap",
-  //     scrub: -1,
-  //     start: "top top",
-  //   },
-  // });
-
-  // const main = document.querySelector(".main");
-
-  // gsap.to(".animate-wrap .info1", {
-  //   x: "0",
-  //   scale: 0.5,
-  //   opacity: 0,
-  //   duration: 1,
-  //   scrollTrigger: {
-  //     trigger: ".info1",
-  //     scrub: -1,
-  //     start: "top top",
-  //     end: () => main.offsetWidth / 2,
-  //   },
-  // });
-
-  // gsap.to(".animate-wrap .info2", {
-  //   x: "0",
-  //   duration: 1,
-  //   scrollTrigger: {
-  //     trigger: ".info1",
-  //     scrub: -1,
-  //     start: "center top",
-  //     end: () => main.offsetWidth / 2,
-  //     pin: true,
-  //   },
-  // });
-
-  // gsap.to(".animate-wrap .info3", {
-  //   x: "0",
-  //   duration: 1,
-  //   scrollTrigger: {
-  //     trigger: ".info2",
-  //     scrub: -1,
-  //     start: "center top",
-  //     end: () => main.offsetWidth / 2,
-  //     pin: true,
-  //   },
-  // });
-
-  // gsap.to(".animate-wrap .info4", {
-  //   x: "0",
-  //   duration: 1,
-  //   scrollTrigger: {
-  //     trigger: ".info3",
-  //     scrub: -1,
-  //     start: "center top",
-  //     end: () => main.offsetWidth / 2,
-  //     pin: true,
-  //   },
-  // });
-
   $(window)
     .scroll(function () {
-      // selectors
       var $window = $(window),
         $body = $(".infos-wrap"),
         $panel = $(".info");
 
-      // Change 33% earlier than scroll position so colour is there when you arrive.
       var scroll = $window.scrollTop() + $window.height() / 3;
 
       $panel.each(function () {
         var $this = $(this);
 
-        // if position is within range of this panel.
-        // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
-        // Remember we set the scroll to 33% earlier in scroll var.
         if (
           $this.position().top <= scroll &&
           $this.position().top + $this.height() > scroll
         ) {
-          // Remove all classes on body with color-
           $body.removeClass(function (index, css) {
             return (css.match(/(^|\s)color-\S+/g) || []).join(" ");
           });
-
-          // Add class of currently active div
           $body.addClass("color-" + $(this).data("color"));
         }
       });
